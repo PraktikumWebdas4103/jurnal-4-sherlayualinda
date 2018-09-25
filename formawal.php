3ecd1a3368bd3c6ecced4bc3ddb5409297cea8e8
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2018-09-25 16:54:56 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,35 @@
+
 <?php
+	session_start();
+	error_reporting(0);
+		$user = array(
+			"user" =>"sherla",
+			"password" =>"sherla"
+		);
+
+	if (isset($_POST['submit'])) {
+		if ($_POST['username']==$user['user'] && $_POST['password']==$user['password']) {
+			$_SESSION['username']= $_POST['username'];
+			echo "Anda berhasil Login $_POST[username]";
+			echo "<a href ='proses.php'>Link</a>";
+			# code...
+		}else {
+			display_login_form();
+			echo "Username atau password anda tidak valid";
+		} 
+
+	}
+	else{
+		display_login_form();
+	}
+	function display_login_form(){ ?>
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method ='POST'>
+			<label for="username">username</label>
+			<input type="text" name="username" id="username">
+			<label for="password">password</label>
+			<input type="password" name="password" id="password">
+			<input type="submit" name="submit" value="submit">
+		</form>
+	<?php } ?>
 
 
-
-?>
--- proses.php
+++ proses.php
@@ -1,4 +1,56 @@
-<?php
+<form action="" method="POST">
+	<table>
+		<tr>
+			<td>
+				Nama	
+			</td>
+			<td>
+					:
+			</td>
+			<td>
+				<input type="text" name="nama">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Hobby
+			</td>
+			<td>
+					:
+			</td>
+			<td>
+				<input type="checkbox" name="hobi[]" value="Makan">Makan 
+				<input type="checkbox" name="hobi[]" value="Minum">Minum 
+				<input type="checkbox" name="hobi[]" value="Tidur">Tidur 
+				<input type="checkbox" name="hobi[]" value="Browsing">Browsing <br><br>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Gambar
+			</td>
+			<td>
+					:
+			</td>
+			<td>
+				<input type="file" name="gambar">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" name="submit" value="Input">
+			</td>	
+		</tr>
+
+	</table>
+</form>
 
 
-?>
+<!-- <?php
+	//if (isset($_POST['submit'])) {
+		//$nama	: $_POST['nama'];
+		//$hobby	: $_POST['hobi'];
+		//# code...
+	}
+	
+?> -->
